6335e36533e3044ce62c07a7f3f38df278a6777b
Leonardo <leo_mc_inter@hotmail.com>
2021-08-11 20:35:02 -0300

    Requisito 7

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Cart from './components/Cart';
+import ProductDetail from './components/ProductDetail';
 
 function App() {
   return (
     <Router>
       <Route exact path="/" component={ Home } />
       <Route exact path="/cart" component={ Cart } />
+      <Route
+        exact
+        path="/product/:id"
+        render={ (props) => <ProductDetail { ...props } /> }
+      />
     </Router>
   );
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class ProductCard extends Component {
   render() {
     const { product } = this.props;
     return (
       <div data-testid="product">
-        <h4>{ product.title }</h4>
+        <h4>{product.title}</h4>
         <img src={ product.thumbnail } alt="Imagem" />
-        <p>{ product.price }</p>
+        <p>{product.price}</p>
+        <Link data-testid="product-detail-link" to={ `/product/${product.id}` }>
+          Ver detalhes
+        </Link>
       </div>
     );
   }
